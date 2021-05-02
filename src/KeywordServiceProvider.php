7aa400fd812451baf73828a87e8288783e19b6ac
Rohan Khatri <khatrirohan80@gmail.com>
2021-05-02 16:47:52 +0530

    loading migrations in service provider

--- src/KeywordServiceProvider.php
+++ src/KeywordServiceProvider.php
@@ -13,15 +13,17 @@ class KeywordServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $modelPath = (file_exists(app_path() . '/Models'))
-            ? base_path('app/Models') : base_path('app');
+//        $modelPath = (file_exists(app_path() . '/Models'))
+//            ? base_path('app/Models') : base_path('app');
+//
+//        // Publishing Model, Traits & Migrations
+//        $this->publishes([
+//            __DIR__ . '/../app'                 => $modelPath,
+//            __DIR__ . '/../app/Traits'          => base_path('app/Traits'),
+//            __DIR__.'/../database/migrations'   => base_path('database/migrations')
+//        ]);
 
-        // Publishing Model, Traits & Migrations
-        $this->publishes([
-            __DIR__ . '/../app'                 => $modelPath,
-            __DIR__ . '/../app/Traits'          => base_path('app/Traits'),
-            __DIR__.'/../database/migrations'   => base_path('database/migrations')
-        ]);
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
     }
 
     /**
